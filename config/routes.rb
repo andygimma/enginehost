c4f218a74e6ec7f4158384158ab62841bb115e12
andygimma <andy.gimma@kiron.ngo>
2017-09-29 14:07:38 -0500

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # get 'static_pages/home'
+  root to: "static_pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  mount Donatable::Engine => "/"
+  mount Donatable::Engine => "/donations"
 end
