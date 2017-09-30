6961083ea49262dcf3ea8a51c66491c94a1a0cd6
andygimma <andy.gimma@kiron.ngo>
2017-09-30 00:12:38 -0500

    commits

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170930043218) do
+ActiveRecord::Schema.define(version: 20170930045906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "fuzzystrmatch"
+  enable_extension "pg_trgm"
 
   create_table "donatable_organizations", force: :cascade do |t|
     t.string "name", null: false
