eb5525cb6b7db16f4d74ae5f3efc48811a00ebed
andygimma <andy.gimma@kiron.ngo>
2017-10-01 17:07:05 -0500

    Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,4 +98,18 @@ Rails.application.configure do
       :secret_access_key => ENV['SECRET_ACCESS_KEY']
     }
   }
+
+  # Setup the mailer config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'donatable.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
 end
